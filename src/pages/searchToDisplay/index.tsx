d362c6072240b9aa96acc43be17bb6e4029b46ca
amitai36 <Amitiy36@gmail.com>
2024-04-30 21:24:56 +0300

    add Carousel with lang

--- src/pages/searchToDisplay/index.tsx
+++ src/pages/searchToDisplay/index.tsx
@@ -1,26 +1,43 @@
+import CarouselComponent from "../../components/Carousel";
 import Search from "../../components/Search";
-import { Box } from "@mui/material";
-
+import searchImage from "../../../public/images/searchImages.png";
+import seeMoreOnMap from "../../../public/images/see_on_map.png";
+import seePhotoStatistics from "../../../public/images/see_photo_statistics.png";
+import seeUserStatistics from "../../../public/images/see_user_statistics.png";
+import showMorePhotoFromArtist from "../../../public/images/show_more_photo_from_artist.png";
+import { useTranslation } from "react-i18next";
 function SearchToDisplay() {
+  const { t } = useTranslation();
+  const photos: { lable: string; imgSrc: string }[] = [
+    { imgSrc: searchImage, lable: t("searchImage") },
+    { imgSrc: seeMoreOnMap, lable: t("seeMoreOnMap") },
+    { imgSrc: showMorePhotoFromArtist, lable: t("showMorePhotoFromArtist") },
+    { imgSrc: seeUserStatistics, lable: t("seeUserStatistics") },
+    { imgSrc: seePhotoStatistics, lable: t("seePhotoStatistics") },
+  ];
   return (
     <div
       style={{
         height: "100%",
-        backgroundImage: "url(../../../public/home_page.jpg)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "calc(100vh - 12%)",
+      <div style={{ height: "20%" }}>
+        <Search />
+      </div>
+      <div
+        style={{
+          direction: "ltr",
+          height: "60%",
+          maxHeight: "60%",
+          width: "60%",
         }}
       >
-        <Search />
-      </Box>
+        <CarouselComponent photos={photos} />
+      </div>
     </div>
   );
 }
